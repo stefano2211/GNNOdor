aa9e9e4a5815604ac96fce86ed2bbafee9163f68
stefano2211 <stefano.andres2004@gmail.com>
2024-10-10 20:57:31 -0400

    "Deleted model.pt file and updated main.py to import and call evaluate_predict_odor instead of evaluate_model"

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import hydra
-from evaluate_model import evaluate_model
+from evaluate_model import evaluate_predict_odor
 from process import process_data
 from train_model import train_model
 
@@ -7,7 +7,7 @@ from train_model import train_model
 @hydra.main(config_path='../config', config_name='main')
 def main(config):
     train_model(config)
-    evaluate_model(config)
+    evaluate_predict_odor(config)
 
  
 if __name__ == "__main__":
