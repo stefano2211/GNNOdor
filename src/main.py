5c2bec11b5bc1e618fc6d845c7ddd26fcdd28b51
stefano2211 <stefano.andres2004@gmail.com>
2024-10-11 17:46:16 -0400

    "Updated hydra.main decorator to include version_base='1.2'"

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from process import process_data
 from train_model import train_model
 
 
-@hydra.main(config_path='../config', config_name='main')
+@hydra.main(config_path='../config', config_name='main', version_base='1.2')
 def main(config):
     train_model(config)
     evaluate_predict_odor(config)
